9495ac8aaba0cba4537bc8b029921f64ae29f6c8
Patrick Drechsler <patrick@pdrechsler.de>
2019-10-14 22:40:51 +0200

    kitty: fix solarized script

--- .config/kitty/switch-to-solarized-light.sh
+++ .config/kitty/switch-to-solarized-light.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 rm ${DIR}/theme.conf
 
-ln -s ${DIR}/kitty-themes/themes/Solarized_Light.conf theme.conf
+ln -s ${DIR}/kitty-themes/themes/Solarized_Light.conf ${DIR}/theme.conf
 
