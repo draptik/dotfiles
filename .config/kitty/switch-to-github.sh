733c6246bb9bd2832b7aa9d285aaca9cdd038b2e
Patrick Drechsler <patrick@pdrechsler.de>
2020-04-02 08:53:41 +0200

    kitty: fix typo in theme softlink

--- .config/kitty/switch-to-github.sh
+++ .config/kitty/switch-to-github.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 rm ${DIR}/theme.conf
 
-ln -s ${DIR}/kitty-themes/themes/GitHub.conf theme.conf
+ln -s ${DIR}/kitty-themes/themes/Github.conf theme.conf
 
