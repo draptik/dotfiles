f9858e321af7b53cf40605130e29c5234872d2b8
Patrick Drechsler <patrick@pdrechsler.de>
2024-02-09 18:04:59 +0100

    🔧 nvim: add custum.lua & upgrade dependencies
    - neotree: close neotree if last window
    - noice: move commandline to bottom

---
+++ .config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true,
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        bottom_search = true,
+      },
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+  },
+}
