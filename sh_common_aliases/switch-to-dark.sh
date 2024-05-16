a0456486db4e90389b1b1c19b10d876fbb6b8736
Patrick Drechsler <patrick@pdrechsler.de>
2024-05-16 15:33:41 +0200

    ✨ btop: add configs (incl theme switching)

--- sh_common_aliases/switch-to-dark.sh
+++ sh_common_aliases/switch-to-dark.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 ln -sf ${DIR}/aliases_theme_dark ${DIR}/themed_aliases
+
+# switch btop's theme
+ln -sf ${DIR}/../.config/btop/btop_dark.conf ${DIR}/../.config/btop/btop.conf
--- sh_common_aliases/switch-to-light.sh
+++ sh_common_aliases/switch-to-light.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-#rm ${DIR}/themed_aliases
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 ln -sf ${DIR}/aliases_theme_light ${DIR}/themed_aliases
 
+# switch btop's theme
+ln -sf ${DIR}/../.config/btop/btop_light.conf ${DIR}/../.config/btop/btop.conf
