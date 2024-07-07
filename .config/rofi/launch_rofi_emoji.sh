c0cde84156cf0a3c4242f75b8bd0ceb57d403306
Patrick Drechsler <patrick@pdrechsler.de>
2024-07-07 20:31:39 +0200

    🐛 rofimoji: fix syntax

--- .config/rofi/launch_rofi_emoji.sh
+++ .config/rofi/launch_rofi_emoji.sh
@@ -3,6 +3,6 @@
 # Requires arch packages rofimoji
 #
 # For some reason the default rofimoji.rc config file does not work.
-# I'll add the "action --copy" here - this works.
+# I'll add the "--action=copy" here - this works.
 #
-rofi -modi "emoji:rofimoji --action copy" -show emoji
+rofi -modi "emoji:rofimoji --action=copy" -show emoji
