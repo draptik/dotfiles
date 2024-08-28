ba30a8369796b91c1474c16bc80929b4201fd4ed
Patrick Drechsler <patrick@pdrechsler.de>
2024-08-28 15:56:27 +0200

    🔧 nvim: enable spell checking for EN and DE

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- required for 'hardtime' plugin
--- vim.opt.showmode = false
--- vim.opt.cmdheight = 2
+vim.opt.spelllang = 'en_us,de_de'
