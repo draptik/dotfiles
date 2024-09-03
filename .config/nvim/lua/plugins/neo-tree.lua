a478a6e8ec4b44580d6c05255209addd3f8702b5
Patrick Drechsler <patrick@pdrechsler.de>
2024-09-03 10:41:11 +0200

    ✨ nvim: always show hidden files/folders in neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -4,6 +4,11 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
     },
   },
 }
