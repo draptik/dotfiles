fb24940319aa71805e2e797203826d7f32355356
Patrick Drechsler <patrick@pdrechsler.de>
2024-09-26 10:04:49 +0200

    💄 nvim(ui): use tokyonight/night theme

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -3,6 +3,7 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     opts = {
+      style = "night",
       -- Make the border color more prominent
       -- For details see https://github.com/folke/tokyonight.nvim/issues/34#issuecomment-2309000368
       on_colors = function(colors)
