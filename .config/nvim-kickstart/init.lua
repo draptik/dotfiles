012efc06fa9deee6eba6a54a9e34abeab56efd76
Patrick Drechsler <patrick@pdrechsler.de>
2024-10-02 21:35:13 +0200

    🚨 nvim/kickstart: suppress linter warning about undefined global 'vim'

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -524,8 +524,12 @@ require('lazy').setup({
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                disable = { 'missing-fields' },
+                -- suppress message `undefined global 'vim'`
+                globals = { 'vim' },
+              },
             },
           },
         },
