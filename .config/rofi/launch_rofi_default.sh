5d37224a5096e01a2cbe03a46cc5338b6d5aca16
Patrick Drechsler <patrick@pdrechsler.de>
2024-10-02 14:40:35 +0200

    🐛 rofi: undo combi (doesn't load path correctly)

--- .config/rofi/launch_rofi_default.sh
+++ .config/rofi/launch_rofi_default.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Application launcher:
-#rofi -show drun
+rofi -show drun
 
 # Application launcher AND script launcher:
-rofi -combi-modi drun,run,combi -show combi -show-icons
+#rofi -combi-modi drun,run,combi -show combi -show-icons
