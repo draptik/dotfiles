b3cb069e4cb16c37760408df10697d6a71b6d6ca
Patrick Drechsler <patrick@pdrechsler.de>
2024-10-03 02:51:18 +0200

    💄sh_common_aliases(lint): shellcheck color switching

--- sh_common_aliases/switch-to-light.sh
+++ sh_common_aliases/switch-to-light.sh
@@ -2,7 +2,7 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-ln -sf ${DIR}/aliases_theme_light ${DIR}/themed_aliases
+ln -sf "${DIR}"/aliases_theme_light "${DIR}"/themed_aliases
 
 # switch btop's theme
-ln -sf ${DIR}/../.config/btop/btop_light.conf ${DIR}/../.config/btop/btop.conf
+ln -sf "${DIR}"/../.config/btop/btop_light.conf "${DIR}"/../.config/btop/btop.conf
