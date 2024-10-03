6e779622edb45dac90230040dae179949ea234c7
Patrick Drechsler <patrick@pdrechsler.de>
2024-10-03 02:25:01 +0200

    ✨ paperless script: move target details to dedicated file

--- scripts/copy-to-paperless.sh
+++ scripts/copy-to-paperless.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Copies file(s) to paperless inbox.
+#
+# Requirements:
+#
+# - `scp` (we could also use `rsync`, but for this scenario it does not make a difference...)
+# - a text file `~/.my-paperless-inbox.txt` containing the name of the paperless inbox.
+#   format: `<username>@<machine-name>:<paperless-inbox-folder-name>`
 
 # If no arguments are provided: return
 if [ $# -eq 0 ]; then
@@ -6,7 +14,22 @@ if [ $# -eq 0 ]; then
   return 1
 fi
 
-PAPERLESS_INBOX="patrick@sheep:paperless-inbox"
+# This file should only contain the name of the VPN connection
+PAPERLESS_FILE="${HOME}/.my-paperless-inbox.txt"
+
+# Check if VPN_FILE exists
+if [ ! -f "$PAPERLESS_FILE" ]; then
+  echo "The file does not exist: $PAPERLESS_FILE"
+  exit 1
+fi
+
+PAPERLESS_INBOX=$(cat "$PAPERLESS_FILE")
+
+# Check if PAPERLESS_INBOX is empty
+if [ -z "$PAPERLESS_INBOX" ]; then
+  echo "The PAPERLESS_INBOX variable is empty."
+  exit 1
+fi
 
 # Pass all input arguments to scp and copy them to the paperless-inbox
 scp "$@" "$PAPERLESS_INBOX/"
