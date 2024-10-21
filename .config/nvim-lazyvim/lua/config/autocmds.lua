48971f8984ab8efe1d283ed1d6ab5cedc9b9c3b1
Patrick Drechsler <patrick@pdrechsler.de>
2024-10-21 12:09:14 +0200

    ✨ nvim: disable auto-formatting of markdown files

--- .config/nvim-lazyvim/lua/config/autocmds.lua
+++ .config/nvim-lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable auto formatting in markdown files
+-- See https://github.com/LazyVim/LazyVim/discussions/141#discussioncomment-9266704
+local set_autoformat = function(pattern, bool_val)
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = pattern,
+    callback = function()
+      vim.b.autoformat = bool_val
+    end,
+  })
+end
+
+set_autoformat({ "markdown" }, false)
