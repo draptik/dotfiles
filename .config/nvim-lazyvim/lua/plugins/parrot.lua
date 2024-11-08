0902589e242f2fdc1b81996bb3b52e7d6e9c57fa
Patrick Drechsler <patrick@pdrechsler.de>
2024-11-08 16:16:29 +0100

    ✨ nvim: add parrot plugin (AI)
    Doesn't work yet with local ollama instance: More research needed.

---
+++ .config/nvim-lazyvim/lua/plugins/parrot.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "frankroeder/parrot.nvim",
+    dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+    opts = {
+      providers = {
+        ollama = {},
+      },
+    },
+  },
+}
