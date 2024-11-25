7c4e4b23be5f3f4f1c06176ec12a7d94454aa1dd
Patrick Drechsler <patrick@pdrechsler.de>
2024-11-25 14:26:21 +0100

    ✨ nvim/lazyvim: enable json plugins

--- .config/nvim-lazyvim/lua/config/lazy.lua
+++ .config/nvim-lazyvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- { import = "lazyvim.plugins.extras.coding.codeium" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
