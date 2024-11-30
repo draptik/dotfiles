a4bae97d95926d4157c197fa8ae542ba13d123e9
Patrick Drechsler <patrick@pdrechsler.de>
2024-11-30 13:10:34 +0100

    🐛 scripts: fix kitty background opacity when switching back to dark theme
    Also fix some linter suggestions...

--- .config/kitty/switch-dark.sh
+++ .config/kitty/switch-dark.sh
@@ -1,27 +1,25 @@
-#!/bin/sh
-echo 'export KITTY_THEME="DARK"' > ~/.zshenv
+#!/bin/bash
+echo 'export KITTY_THEME="DARK"' >~/.zshenv
 
 # This script must be executed from within kitty
 # Also see the `background-opacity` value in the currently used `kitty.conf`
-kitty @ set-background-opacity 0.8
+kitty @ set-background-opacity 1
 
 ## This line must be executed from within kitty
 kitty @ set-colors -a ~/.config/kitty/kitty-themes/themes/Tango_Dark.conf
 
 ln -sf \
-    ~/.dotfiles/.config/kitty/kitty-themes/themes/Tango_Dark.conf \
-    ~/.dotfiles/.config/kitty/theme.conf
+  ~/.dotfiles/.config/kitty/kitty-themes/themes/Tango_Dark.conf \
+  ~/.dotfiles/.config/kitty/theme.conf
 
 ~/.dotfiles/sh_common_aliases/switch-to-dark.sh
 
 ~/.dotfiles/.config/ranger/switch-to-kitty-dark.sh
 
-
 ## Reload zsh configs
 ##
 ## "Sourcing" `source ~/.zshrc` didn't work. Use `exec /bin/zsh` instead:
-exec /bin/zsh 
+exec /bin/zsh
 
 ## Same for bash...
 exec /bin/bash
-
