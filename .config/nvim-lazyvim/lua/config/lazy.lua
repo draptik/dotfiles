79d357e05edfdff82877f5846f1ce18b60d0aee4
Patrick Drechsler <patrick@pdrechsler.de>
2024-12-20 13:59:49 +0100

    ✨ feat(nvim/lazyvim): replace neotree with mini-files

--- .config/nvim-lazyvim/lua/config/lazy.lua
+++ .config/nvim-lazyvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
