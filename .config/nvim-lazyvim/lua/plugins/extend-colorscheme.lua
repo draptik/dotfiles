97af3f754d910e55e42a6e64bee56841dbfafacb
Patrick Drechsler <patrick@pdrechsler.de>
2025-03-20 16:35:31 +0100

    🔧 chore(nvim/lazyvim): back to dark mode...

--- .config/nvim-lazyvim/lua/plugins/extend-colorscheme.lua
+++ .config/nvim-lazyvim/lua/plugins/extend-colorscheme.lua
@@ -3,7 +3,7 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     opts = {
-      style = "day",
+      style = "night",
       -- Make the border color more prominent
       -- For details see https://github.com/folke/tokyonight.nvim/issues/34#issuecomment-2309000368
       on_colors = function(colors)
