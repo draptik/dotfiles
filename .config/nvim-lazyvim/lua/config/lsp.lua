50bb115b0a7b43e350c59117a13288de4a13b612
Patrick Drechsler <patrick@pdrechsler.de>
2025-04-01 16:49:53 +0200

    🔧 chore(nvim/lazyvim): suppress 'undefined global `vim`' warning

---
+++ .config/nvim-lazyvim/lua/config/lsp.lua
@@ -0,0 +1,13 @@
+require("lspconfig").lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+    },
+  },
+})
