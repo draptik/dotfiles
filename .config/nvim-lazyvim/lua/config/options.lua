8860b4e6419afe06fc449e5929cc8cd41f11553b
Patrick Drechsler <patrick@pdrechsler.de>
2025-04-08 11:51:05 +0200

    💄 chore(nvim/lazyvim): extend markdown-render to allow 'csharp' in addition to 'cs'

--- .config/nvim-lazyvim/lua/config/options.lua
+++ .config/nvim-lazyvim/lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.opt.spelllang = "en_us,de_de,my-custom"
+
+-- Enables c# syntax highlighting in markdown code blocks
+-- NOTE: `c_sharp` is the treesitter name, `csharp` is the name of the new alias.
+-- NOTE: The default name in markdown code blocks is `cs`.
+vim.treesitter.language.register("c_sharp", "csharp")
---
+++ .config/nvim-lazyvim/lua/plugins/extend-mini-icons.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    -- When using `csharp` in markdown code blocks, use the correct icon
+    filetype = {
+      csharp = {
+        glyph = "󰌛",
+        hl = "MiniIconsGreen",
+      },
+    },
+  },
+}
