2b65349c10ba628591840b37cdb024688eedbe0f
Patrick Drechsler <patrick@pdrechsler.de>
2025-06-25 21:12:56 +0200

    🔧 chore(nvim): disable relative line numbers (I don't use it for navigation)

--- .config/nvim-lazyvim/lua/config/options.lua
+++ .config/nvim-lazyvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable relative line numbers because I don't need them for navigation.
+vim.opt.relativenumber = false
+
 vim.opt.spelllang = "en_us,de_de,my-custom"
 
 -- Enables c# syntax highlighting in markdown code blocks
