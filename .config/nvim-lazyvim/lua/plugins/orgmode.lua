c3cc2b5749349c0b9e8a44c8f11d9f8b36f7c9eb
Patrick Drechsler <patrick@pdrechsler.de>
2026-01-13 22:26:18 +0100

    🔧 chore(nvim): remap org_todo keybindings bc conflict w/ lazyvim

--- .config/nvim-lazyvim/lua/plugins/orgmode.lua
+++ .config/nvim-lazyvim/lua/plugins/orgmode.lua
@@ -8,6 +8,19 @@ return {
     org.setup({
       org_agenda_files = "~/tmp/org-demo/**/*",
       org_default_notes_file = "~/tmp/org-demo/mydefault.org",
+
+      -- The default keybindings are located here:
+      -- https://github.com/nvim-orgmode/orgmode/blob/master/lua/orgmode/config/defaults.lua
+      mappings = {
+        org = {
+          -- default keybindings (i.e. for `org_todo` and `org_todo_prev`) seem to be
+          -- in conflict with LazyVim's defaults:
+          org_todo = "<Leader>os",
+          org_todo_prev = "<Leader>oS",
+          org_priority_up = "<Leader>oP",
+          org_priority_down = "<Leader>op",
+        },
+      },
     })
   end,
 }
