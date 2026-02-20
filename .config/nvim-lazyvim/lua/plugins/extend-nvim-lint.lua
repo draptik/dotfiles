84eff1c12c3cbc630ae929ded6e7ea0893212bcd
Patrick Drechsler <patrick@pdrechsler.de>
2026-02-20 16:38:50 +0100

    🔧 chore(nvim): update syntax for markdownlint-cli2

--- .config/nvim-lazyvim/lua/plugins/extend-nvim-lint.lua
+++ .config/nvim-lazyvim/lua/plugins/extend-nvim-lint.lua
@@ -7,8 +7,9 @@ return {
     },
     linters = {
       -- For details see https://github.com/LazyVim/LazyVim/discussions/4094
+      -- and https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-15525364
       ["markdownlint-cli2"] = {
-        args = { "--config", HOME .. "/.dotfiles/.config/markdownlint/.markdownlint-cli2.yaml", "--" },
+        prepend_args = { "--config", HOME .. "/.dotfiles/.config/markdownlint/.markdownlint-cli2.yaml", "--" },
       },
     },
   },
