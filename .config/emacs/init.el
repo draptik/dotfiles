de4696a42fcc6a999a7fff7d93812a11864a4372
Patrick Drechsler <patrick@pdrechsler.de>
2026-03-19 20:21:07 +0100

    ⚰️ chore(emacs): remove unused modes and theme

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -11,22 +11,6 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; (message "Deleting old backup files...")
-;; (let ((week (* 60 60 24 7))
-;;       (current (float-time (current-time))))
-;;   (dolist (file (directory-files temporary-file-directory t))
-;;     (when (and (backup-file-name-p file)
-;;                (> (- current (float-time (fifth (file-attributes file))))
-;;                   week))
-;;       (message "%s" file)
-;;       (delete-file file))))
-
-
-; YAML Mode
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
-
 ;; ===================================================================
 ;; DISABLE CTRL-Z MINIMIZATION/SUSPENSION OF EMACS ===================
 ;; ===================================================================
@@ -103,7 +87,6 @@
  ;; If there is more than one, they won't work right.
  '(calendar-date-style 'european)
  '(column-number-mode t)
- '(custom-enabled-themes '(tango-dark))
  '(desktop-restore-eager 5)
  '(desktop-save-mode t)
  '(display-time-24hr-format t)
@@ -113,8 +96,6 @@
  '(ido-enable-flex-matching t)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-screen t)
- '(package-selected-packages
-   '(haskell-mode markdown-mode fsharp-mode i3wm-config-mode csharp-mode ## yaml-mode))
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil))
