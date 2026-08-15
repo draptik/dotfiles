5d5f40d8b2ad3799189b3d8f0848eaf92d5dabe6
Patrick Drechsler <patrick@pdrechsler.de>
2026-08-16 01:02:48 +0200

    🔧 chore(yay): enable checks again

--- .config/yay/init.lua
+++ .config/yay/init.lua
@@ -1,5 +1,5 @@
--- require("hooks.maintainer_change")
---require("hooks.recently_modified")
+require("hooks.maintainer_change")
+require("hooks.recently_modified")
 
 ---@diagnostic disable: undefined-global
 yay.opt.answer_diff = "ALL"
