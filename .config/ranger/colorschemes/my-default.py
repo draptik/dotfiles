e2c524f0d66eef60bb7c7cebcbb9d186330b4e61
Patrick Drechsler <patrick@pdrechsler.de>
2026-08-17 14:40:31 +0200

    🔧 chore(ranger): improve readability of directory names
    Derive my-default theme from default theme and patch directory color
    manually.

---
+++ .config/ranger/colorschemes/my-default.py
@@ -0,0 +1,25 @@
+# This file is part of ranger, the console file manager.
+# License: GNU GPL version 3, see the file "AUTHORS" for details.
+
+from __future__ import absolute_import, division, print_function
+
+from ranger.colorschemes.default import Default
+from ranger.gui.color import green, blue, bold
+
+
+class Scheme(Default):
+    progress_bar_color = green
+
+    def use(self, context):
+        fg, bg, attr = Default.use(self, context)
+
+        if (
+            context.directory
+            and not context.marked
+            and not context.link
+            and not context.inactive_pane
+        ):
+            fg = blue
+            attr |= bold
+
+        return fg, bg, attr
